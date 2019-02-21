2db0fb9f6ce07612040f3ad914fa97ac8b859eb5
nicholas mtabo <nicholasmtabo@yahoo.com>
2019-02-21 12:08:26 +0300

    setup privateRoute access handler

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../components/Header';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import NotFoundPage from '../components/NotFound';
 import HelpPage from '../components/Help';
 import EditExpensePage from '../components/EditExpense';
 import AddExpensePage from '../components/AddExpense';
 import ExpenseDashboardPage from '../components/Dashboard';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
 
+export const history = createHistory()
+
+// use Router instead of BrowserRouter to use customized history
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
